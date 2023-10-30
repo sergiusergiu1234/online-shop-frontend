5defb20c6b86165d2f7ffdff342388d6d8d1b934
STEfAN SERGIU <sergiuc.stefan@gmail.com>
2023-10-30 17:42:11 +0200

    modified many stuff

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
  //export const API_URL = 'http://localhost:8080';
-export const ${API_URL}='https://slope-emporium-app-b7686b574df7.herokuapp.com'
+export const API_URL='https://slope-emporium-app-b7686b574df7.herokuapp.com'
 
 
 export const fetchTypes = async () => {
