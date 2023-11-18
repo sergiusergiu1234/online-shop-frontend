4a7a48e07e0c06bb5c384f62182ef6dc5e5cf495
STEfAN SERGIU <sergiuc.stefan@gmail.com>
2023-11-18 15:41:30 +0200

    changes for better performance and design

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import { error } from "console";
 import { Attribute } from "../Types/Attribute.types";
 
-export const API_URL = 'http://localhost:8080';
-//export const API_URL='https://slope-emporium-app-b7686b574df7.herokuapp.com'
+//export const API_URL = 'http://localhost:8080';
+export const API_URL='https://slope-emporium-app-b7686b574df7.herokuapp.com'
 
 
 export const fetchTypes = async () => {
