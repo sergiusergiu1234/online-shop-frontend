56b05f4bc5ca07230d28e4766830866af3b99c22
STEfAN SERGIU <sergiuc.stefan@gmail.com>
2023-11-21 13:17:05 +0200

    s

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <li><a href="https://slope-emporium.vercel.app/contact"><MdEmail/> Email</a></li>
             <li><a href="https://www.linkedin.com/in/sergiu-catalin-stefan-a5553a196/"><BsLinkedin/> LinkedIn</a></li>
             <li><a href="https://github.com/sergiusergiu1234?tab=repositories"><BsGithub/>GitHub</a></li>
-            <li><a href="https://sergiusergiu1234.github.io/page/"><BsGlobe/> Portfolio</a></li>
+            <li><a href="https://portfolio-liard-two-36.vercel.app/"><BsGlobe/> Portfolio</a></li>
         </IconContext.Provider>
         </ul>
       </div>
